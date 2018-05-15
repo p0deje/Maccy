c595bacc7a6b11f917a9a2509865f14dccfa6234
Alex Rodionov <p0deje@gmail.com>
2018-05-15 15:50:17 +0600

    Slow UI tests down a bit in attempt to stabilize CI

--- MaccyUITests/MaccyUITests.swift
+++ MaccyUITests/MaccyUITests.swift
@@ -8,22 +8,22 @@ class MaccyUITests: XCTestCase {
   let copy1 = UUID().uuidString
   let copy2 = UUID().uuidString
   let copy3 = UUID().uuidString
-  
+
   var popUpEvents: [CGEvent] {
     let eventDown = CGEvent(keyboardEventSource: nil, virtualKey: UInt16(kVK_ANSI_C), keyDown: true)!
-    eventDown.flags = [CGEventFlags.maskCommand,CGEventFlags.maskShift]
-    
+    eventDown.flags = [CGEventFlags.maskCommand, CGEventFlags.maskShift]
+
     let eventUp = CGEvent(keyboardEventSource: nil, virtualKey: UInt16(kVK_ANSI_C), keyDown: false)!
-    eventUp.flags = [CGEventFlags.maskCommand,CGEventFlags.maskShift]
-    
+    eventUp.flags = [CGEventFlags.maskCommand, CGEventFlags.maskShift]
+
     return [eventDown, eventUp]
   }
-  
+
   override func setUp() {
     super.setUp()
     continueAfterFailure = false
     app.launch()
-    
+
     pasteboard.declareTypes([NSPasteboard.PasteboardType.string], owner: nil)
     copyToClipboard("foo") // the first item is not copied for some reason
     copyToClipboard(copy1)
@@ -34,38 +34,38 @@ class MaccyUITests: XCTestCase {
   func testSearchAndCopy() {
     popUp()
     app.typeText(copy1)
-    app.typeKey(.enter, modifierFlags: [])
+    typeKey(.enter)
     XCTAssertEqual(pasteboard.string(forType: NSPasteboard.PasteboardType.string), copy1)
-    
+
     popUp()
     app.typeText(copy3)
-    app.typeKey(.enter, modifierFlags: [])
+    typeKey(.enter)
     XCTAssertEqual(pasteboard.string(forType: NSPasteboard.PasteboardType.string), copy3)
 
     popUp()
     app.typeText(copy2)
-    app.typeKey(.enter, modifierFlags: [])
+    typeKey(.enter)
     XCTAssertEqual(pasteboard.string(forType: NSPasteboard.PasteboardType.string), copy2)
   }
-  
+
   func testSelectWithArrowKeysAndCopy() {
     popUp()
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.enter, modifierFlags: [])
+    typeKey(.downArrow)
+    typeKey(.downArrow)
+    typeKey(.enter)
     XCTAssertEqual(pasteboard.string(forType: NSPasteboard.PasteboardType.string), copy2)
-    
+
     popUp()
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.enter, modifierFlags: [])
+    typeKey(.downArrow)
+    typeKey(.downArrow)
+    typeKey(.enter)
     XCTAssertEqual(pasteboard.string(forType: NSPasteboard.PasteboardType.string), copy3)
-    
+
     popUp()
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.downArrow, modifierFlags: [])
-    app.typeKey(.enter, modifierFlags: [])
+    typeKey(.downArrow)
+    typeKey(.downArrow)
+    typeKey(.downArrow)
+    typeKey(.enter)
     XCTAssertEqual(pasteboard.string(forType: NSPasteboard.PasteboardType.string), copy1)
   }
 
@@ -74,10 +74,15 @@ class MaccyUITests: XCTestCase {
       event.post(tap: CGEventTapLocation.cghidEventTap)
     }
   }
-  
+
   private func copyToClipboard(_ content: String) {
     pasteboard.clearContents()
     pasteboard.setString(content, forType: NSPasteboard.PasteboardType.string)
-    sleep(2) // make sure Maccy knows about new item
+    sleep(3) // make sure Maccy knows about new item
+  }
+
+  private func typeKey(_ key: XCUIKeyboardKey) {
+    app.typeKey(key, modifierFlags: [])
+    sleep(1) // give Maccy some time to process key
   }
 }
