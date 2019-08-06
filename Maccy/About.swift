8b1cd9539f39dce9bc541d903894666997c285e6
Alex Rodionov <p0deje@gmail.com>
2019-08-06 12:24:11 +0600

    Kudos to Sasha Koss

--- Maccy/About.swift
+++ Maccy/About.swift
@@ -1,16 +1,31 @@
 import Cocoa
 
 class About {
-  private let defaultCreditsAttributes = [NSAttributedString.Key.foregroundColor: NSColor.labelColor]
-  private let familyCreditsText = "Special thank you to Tonia & Guy! ❤️"
+  private let familyCredits = NSAttributedString(
+    string: "Special thank you to Tonia & Guy! ❤️",
+    attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor]
+  )
+
+  private var kossCredits: NSMutableAttributedString {
+    let string = NSMutableAttributedString(string: "Kudos to Sasha Koss for help! 🏂",
+                                           attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+    string.addAttribute(.link, value: "https://koss.nocorp.me", range: NSRange(location: 9, length: 10))
+    return string
+  }
+
+  private var credits: NSMutableAttributedString {
+    let credits = NSMutableAttributedString(string: "",
+                                            attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+    credits.append(kossCredits)
+    credits.append(NSAttributedString(string: "\n"))
+    credits.append(familyCredits)
+    credits.setAlignment(.center, range: NSRange(location: 0, length: credits.length))
+    return credits
+  }
 
   @objc
   func openAbout(_ sender: NSMenuItem) {
     NSApp.activate(ignoringOtherApps: true)
-
-    let credits = NSMutableAttributedString()
-    credits.append(NSAttributedString(string: familyCreditsText, attributes: defaultCreditsAttributes))
-
     NSApp.orderFrontStandardAboutPanel(options: [NSApplication.AboutPanelOptionKey.credits: credits])
   }
 }
