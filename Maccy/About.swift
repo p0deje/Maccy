39b6a6fcd9a106a3d6ca8a09e93ff2dbfdbb6cdd
Alex Rodionov <p0deje@gmail.com>
2020-09-14 11:26:39 -0400

    Add links in about window

--- Maccy/About.swift
+++ Maccy/About.swift
@@ -13,9 +13,20 @@ class About {
     return string
   }
 
+  private var links: NSMutableAttributedString {
+    let string = NSMutableAttributedString(string: "Website│GitHub│Support",
+                                           attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+    string.addAttribute(.link, value: "https://maccy.app", range: NSRange(location: 0, length: 7))
+    string.addAttribute(.link, value: "https://github.com/p0deje/Maccy", range: NSRange(location: 8, length: 6))
+    string.addAttribute(.link, value: "mailto:support@maccy.app", range: NSRange(location: 15, length: 7))
+    return string
+  }
+
   private var credits: NSMutableAttributedString {
     let credits = NSMutableAttributedString(string: "",
                                             attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+    credits.append(links)
+    credits.append(NSAttributedString(string: "\n\n"))
     credits.append(kossCredits)
     credits.append(NSAttributedString(string: "\n"))
     credits.append(familyCredits)
