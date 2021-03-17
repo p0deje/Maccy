6e9405e4e911f483644e65a36bc55bce643d7ea6
Alex Rodionov <p0deje@gmail.com>
2021-03-17 14:41:00 -0700

    Limit fuzzy search to 5000 characters

--- Maccy/Search.swift
+++ Maccy/Search.swift
@@ -2,9 +2,15 @@ import AppKit
 import Fuse
 
 class Search {
+  struct FuzzySearchResult {
+    var score: Double?
+    var object: Menu.IndexedItem
+  }
+
   typealias Searchable = [Menu.IndexedItem]
 
   private let fuse = Fuse(threshold: 0.7) // threshold found by trial-and-error
+  private let fuzzySearchLimit = 5_000
 
   func search(string: String, within: Searchable) -> Searchable {
     guard !string.isEmpty else {
@@ -20,11 +26,21 @@ class Search {
 
   private func fuzzySearch(string: String, within: Searchable) -> Searchable {
     let pattern = fuse.createPattern(from: string)
-    let searchResults = within.map({
-      (score: fuse.search(pattern, in: $0.value)?.score, object: $0)
+    let searchResults: [FuzzySearchResult] = within.compactMap({ item in
+      var searchString = item.value
+      if searchString.count > fuzzySearchLimit {
+        // shortcut to avoid slow search
+        let stopIndex = searchString.index(searchString.startIndex, offsetBy: fuzzySearchLimit)
+        searchString = "\(searchString[...stopIndex])"
+      }
+
+      if let fuzzyScore = fuse.search(pattern, in: searchString)?.score {
+        return FuzzySearchResult(score: fuzzyScore, object: item)
+      } else {
+        return nil
+      }
     })
-    let matchedResults = searchResults.filter({ $0.score != nil })
-    let sortedResults = matchedResults.sorted(by: { ($0.score ?? 0) < ($1.score ?? 0) })
+    let sortedResults = searchResults.sorted(by: { ($0.score ?? 0) < ($1.score ?? 0) })
     return sortedResults.map({ $0.object })
   }
 
