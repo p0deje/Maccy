p0deje/Maccy

commit f49eec691b182c4d678930e0941ca890a8b52dd5

Highlight search matches with bold font
Closes #289

Maccy/Menu.swift
@@ -133,14 +133,15 @@ class Menu: NSMenu, NSMenuDelegate {
     }
 
     // Get all the menu items that match results.
-    var foundMenuItems = results.flatMap({ $0.menuItems })
+    let foundItems = results.map({ $0.object })
+    var foundMenuItems = foundItems.flatMap({ $0.menuItems })
     if filter.isEmpty {
       foundMenuItems.append(contentsOf: indexedItems.flatMap({ $0.menuItems }).filter({ $0.isPinned }))
     }
 
     // First, remove items that don't match search.
     for indexedItem in indexedItems {
-      if !results.contains(indexedItem) {
+      if !foundItems.contains(indexedItem) {
         for menuItem in indexedItem.menuItems where items.contains(menuItem) {
           removeItem(menuItem)
         }
@@ -148,11 +149,14 @@ class Menu: NSMenu, NSMenuDelegate {
     }
 
     // Second, update order of items to match search results order.
-    for menuItem in foundMenuItems.reversed() {
-      if items.contains(menuItem) {
-        removeItem(menuItem)
+    for result in results.reversed() {
+      for menuItem in result.object.menuItems.reversed() {
+        if items.contains(menuItem) {
+          removeItem(menuItem)
+        }
+        menuItem.highlight(result.matches)
+        insertItem(menuItem, at: historyMenuItemOffset)
       }
-      insertItem(menuItem, at: historyMenuItemOffset)
     }
 
     setKeyEquivalents(historyMenuItems)

Maccy/HistoryMenuItem.swift
@@ -10,6 +10,7 @@ class HistoryMenuItem: NSMenuItem {
   private let showMaxLength = 50
   private let tooltipMaxLength = 5_000
   private let imageMaxWidth: CGFloat = 340.0
+  private let highlightFont = NSFont.boldSystemFont(ofSize: NSFont.systemFontSize)
 
   required init(coder: NSCoder) {
     super.init(coder: coder)
@@ -75,6 +76,40 @@ class HistoryMenuItem: NSMenuItem {
     loadImage(item)
   }
 
+  func highlight(_ ranges: [ClosedRange<Int>]) {
+    guard !ranges.isEmpty else {
+      self.attributedTitle = nil
+      return
+    }
+
+    var trimmedOffset = 0
+    for char in value {
+      if char.isWhitespace || char.isNewline {
+        trimmedOffset += 1
+      } else {
+        break
+      }
+    }
+
+    let attributedTitle = NSMutableAttributedString(string: title)
+    for range in ranges {
+      let lowerTrimmedBound = range.lowerBound - trimmedOffset
+      let upperTrimmedBound = range.upperBound - trimmedOffset
+      let rangeLength = upperTrimmedBound - lowerTrimmedBound + 1
+
+      if lowerTrimmedBound >= 0 && rangeLength >= 0 {
+        let highlightRange = NSRange(location: lowerTrimmedBound, length: rangeLength)
+
+        if Range(highlightRange, in: title) != nil {
+          attributedTitle.addAttribute(.font, value: highlightFont, range: highlightRange
+          )
+        }
+      }
+    }
+
+    self.attributedTitle = attributedTitle
+  }
+
   private func isImage(_ item: HistoryItem) -> Bool {
     return contentData(item, [.tiff, .png]) != nil
   }
@@ -124,6 +159,7 @@ class HistoryMenuItem: NSMenuItem {
       }
     }
   }
+
   private func loadString(_ item: HistoryItem, from: NSPasteboard.PasteboardType) {
     if let contentData = contentData(item, [from]) {
       if let string = String(data: contentData, encoding: .utf8) {

Maccy/Search.swift
@@ -2,9 +2,10 @@ import AppKit
 import Fuse
 
 class Search {
-  struct FuzzySearchResult {
+  struct SearchResult: Equatable {
     var score: Double?
     var object: Menu.IndexedItem
+    var matches: [ClosedRange<Int>]
   }
 
   typealias Searchable = [Menu.IndexedItem]
@@ -12,9 +13,9 @@ class Search {
   private let fuse = Fuse(threshold: 0.7) // threshold found by trial-and-error
   private let fuzzySearchLimit = 5_000
 
-  func search(string: String, within: Searchable) -> Searchable {
+  func search(string: String, within: Searchable) -> [SearchResult] {
     guard !string.isEmpty else {
-      return within
+      return within.map({ SearchResult(score: nil, object: $0, matches: [])})
     }
 
     if UserDefaults.standard.fuzzySearch {
@@ -24,9 +25,9 @@ class Search {
     }
   }
 
-  private func fuzzySearch(string: String, within: Searchable) -> Searchable {
+  private func fuzzySearch(string: String, within: Searchable) -> [SearchResult] {
     let pattern = fuse.createPattern(from: string)
-    let searchResults: [FuzzySearchResult] = within.compactMap({ item in
+    let searchResults: [SearchResult] = within.compactMap({ item in
       var searchString = item.value
       if searchString.count > fuzzySearchLimit {
         // shortcut to avoid slow search
@@ -34,26 +35,39 @@ class Search {
         searchString = "\(searchString[...stopIndex])"
       }
 
-      if let fuzzyScore = fuse.search(pattern, in: searchString)?.score {
-        return FuzzySearchResult(score: fuzzyScore, object: item)
+      if let fuzzyResult = fuse.search(pattern, in: searchString) {
+        return SearchResult(
+          score: fuzzyResult.score,
+          object: item,
+          matches: fuzzyResult.ranges
+        )
       } else {
         return nil
       }
     })
     let sortedResults = searchResults.sorted(by: { ($0.score ?? 0) < ($1.score ?? 0) })
-    return sortedResults.map({ $0.object })
+
+    return sortedResults
   }
 
-  private func simpleSearch(string: String, within: Searchable) -> Searchable {
-    return within.filter({ item in
-      let range = item.value.range(
+  private func simpleSearch(string: String, within: Searchable) -> [SearchResult] {
+    return within.compactMap({ item in
+      if let range = item.value.range(
         of: string,
         options: .caseInsensitive,
         range: nil,
         locale: nil
-      )
-
-      return (range != nil)
+      ) {
+        let lowerBound = item.value.distance(from: item.value.startIndex, to: range.lowerBound)
+        let upperBound = item.value.distance(from: item.value.startIndex, to: range.upperBound) - 1
+        return SearchResult(
+          score: nil,
+          object: item,
+          matches: [lowerBound...upperBound]
+        )
+      } else {
+        return nil
+      }
     })
   }
 }

MaccyTests/HistoryMenuItemTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import Maccy
 
 class HistoryMenuItemTests: XCTestCase {
+  let boldFont = NSFont.boldSystemFont(ofSize: NSFont.systemFontSize)
   let savedImageMaxHeight = UserDefaults.standard.imageMaxHeight
 
   var firstCopiedAt: Date! {
@@ -152,6 +153,40 @@ class HistoryMenuItemTests: XCTestCase {
     let menuItem = historyMenuItem(String(repeating: "a", count: 5_001))
     XCTAssertEqual(menuItem.toolTip,
             tooltip("\(String(repeating: "a", count: 3_333))...\(String(repeating: "a", count: 1_667))"))
+  }
+
+  func testHighlightTitle() {
+    let menuItem = historyMenuItem("foo bar baz")
+    menuItem.highlight([4...6, 8...9])
+    let expectedTitle = NSMutableAttributedString(string: "foo bar baz")
+    expectedTitle.addAttribute(.font, value: boldFont, range: NSRange(location: 4, length: 3))
+    expectedTitle.addAttribute(.font, value: boldFont, range: NSRange(location: 8, length: 2))
+    XCTAssertEqual(menuItem.attributedTitle, expectedTitle)
+    menuItem.highlight([])
+    XCTAssertEqual(menuItem.attributedTitle, nil)
+  }
+
+  func testHighlightTrimmedTitle() {
+    let menuItem = historyMenuItem(" \n foo bar baz")
+    menuItem.highlight([7...9])
+    let expectedTitle = NSMutableAttributedString(string: "foo bar baz")
+    expectedTitle.addAttribute(.font, value: boldFont, range: NSRange(location: 4, length: 3))
+    XCTAssertEqual(menuItem.attributedTitle, expectedTitle)
+  }
+
+  func testHighlightTimmedPartOfTitle() {
+    let menuItem = historyMenuItem(" \n foo bar baz")
+    menuItem.highlight([1...2])
+    let expectedTitle = NSMutableAttributedString(string: "foo bar baz")
+    XCTAssertEqual(menuItem.attributedTitle, expectedTitle)
+  }
+
+  func testHighlightHiddenPartOfTitle() {
+    let menuItem = historyMenuItem("foo bar baz")
+    menuItem.highlight([4...6, 99...100])
+    let expectedTitle = NSMutableAttributedString(string: "foo bar baz")
+    expectedTitle.addAttribute(.font, value: boldFont, range: NSRange(location: 4, length: 3))
+    XCTAssertEqual(menuItem.attributedTitle, expectedTitle)
   }
 
   private func historyMenuItem(_ value: String?) -> HistoryMenuItem {

MaccyTests/SearchTests.swift
@@ -24,11 +24,26 @@ class SearchTests: XCTestCase {
   func testSimpleSearch() {
     UserDefaults.standard.fuzzySearch = false
 
-    XCTAssertEqual(search(""), items)
-    XCTAssertEqual(search("z"), items)
-    XCTAssertEqual(search("foo"), [items[0], items[1]])
-    XCTAssertEqual(search("za"), [items[1]])
-    XCTAssertEqual(search("yyy"), [items[2]])
+    XCTAssertEqual(search(""), [
+      Search.SearchResult(score: nil, object: items[0], matches: []),
+      Search.SearchResult(score: nil, object: items[1], matches: []),
+      Search.SearchResult(score: nil, object: items[2], matches: [])
+    ])
+    XCTAssertEqual(search("z"), [
+      Search.SearchResult(score: nil, object: items[0], matches: [10...10]),
+      Search.SearchResult(score: nil, object: items[1], matches: [8...8]),
+      Search.SearchResult(score: nil, object: items[2], matches: [8...8])
+    ])
+    XCTAssertEqual(search("foo"), [
+      Search.SearchResult(score: nil, object: items[0], matches: [0...2]),
+      Search.SearchResult(score: nil, object: items[1], matches: [0...2])
+    ])
+    XCTAssertEqual(search("za"), [
+      Search.SearchResult(score: nil, object: items[1], matches: [8...9])
+    ])
+    XCTAssertEqual(search("yyy"), [
+      Search.SearchResult(score: nil, object: items[2], matches: [4...6])
+    ])
     XCTAssertEqual(search("fbb"), [])
     XCTAssertEqual(search("m"), [])
   }
@@ -36,16 +51,36 @@ class SearchTests: XCTestCase {
   func testFuzzySearch() {
     UserDefaults.standard.fuzzySearch = true
 
-    XCTAssertEqual(search(""), items)
-    XCTAssertEqual(search("z"), [items[1], items[2], items[0]])
-    XCTAssertEqual(search("foo"), [items[0], items[1]])
-    XCTAssertEqual(search("za"), [items[1], items[0], items[2]])
-    XCTAssertEqual(search("yyy"), [items[2]])
-    XCTAssertEqual(search("fbb"), [items[0], items[1]])
+    XCTAssertEqual(search(""), [
+      Search.SearchResult(score: nil, object: items[0], matches: []),
+      Search.SearchResult(score: nil, object: items[1], matches: []),
+      Search.SearchResult(score: nil, object: items[2], matches: [])
+    ])
+    XCTAssertEqual(search("z"), [
+      Search.SearchResult(score: 0.08, object: items[1], matches: [8...8, 10...10]),
+      Search.SearchResult(score: 0.08, object: items[2], matches: [8...10]),
+      Search.SearchResult(score: 0.1, object: items[0], matches: [10...10])
+    ])
+    XCTAssertEqual(search("foo"), [
+      Search.SearchResult(score: 0.0, object: items[0], matches: [0...2]),
+      Search.SearchResult(score: 0.0, object: items[1], matches: [0...2])
+    ])
+    XCTAssertEqual(search("za"), [
+      Search.SearchResult(score: 0.08, object: items[1], matches: [5...5, 8...9]),
+      Search.SearchResult(score: 0.54, object: items[0], matches: [5...5, 9...10]),
+      Search.SearchResult(score: 0.58, object: items[2], matches: [8...10])
+    ])
+    XCTAssertEqual(search("yyy"), [
+      Search.SearchResult(score: 0.04, object: items[2], matches: [4...6])
+    ])
+    XCTAssertEqual(search("fbb"), [
+      Search.SearchResult(score: 0.6666666666666666, object: items[0], matches: [0...0, 4...4, 8...8]),
+      Search.SearchResult(score: 0.6666666666666666, object: items[1], matches: [0...0, 4...4])
+    ])
     XCTAssertEqual(search("m"), [])
   }
 
-  private func search(_ string: String) -> Search.Searchable {
+  private func search(_ string: String) -> [Search.SearchResult] {
     return Search().search(string: string, within: items)
   }
 }

commit 0644fdb88beeaa0b79ed1e33c990758dd1db3b61

Fix maxMenuItems + bottom pinned items invisible on search clear
Regression in f49eec6

Maccy/Menu.swift
@@ -128,11 +128,13 @@ class Menu: NSMenu, NSMenuDelegate {
       results = Array(results[0...maxMenuItems - 1])
     }
 
-    // Get all the menu items that match results.
+    // Get all the items that match results.
     let foundItems = results.map({ $0.object })
-    var foundMenuItems = foundItems.flatMap({ $0.menuItems })
+
+    // Ensure that pinned items are visible after search is cleared.
     if filter.isEmpty {
-      foundMenuItems.append(contentsOf: indexedItems.flatMap({ $0.menuItems }).filter({ $0.isPinned }))
+      results.append(contentsOf: indexedItems.filter({ $0.item.pin != nil })
+                                             .map({ Search.SearchResult(score: nil, object: $0, matches: []) }))
     }
 
     // First, remove items that don't match search.