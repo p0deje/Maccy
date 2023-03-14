5ce912d374c2117860625836714c1f3aa329d6c3
Alex Rodionov <p0deje@gmail.com>
2023-03-14 14:14:14 -0700

    Fix pasting to work with "bépo 1.1 - Azerty ⌘" layout
    Fixes #520

--- Maccy/KeyboardLayout.swift
+++ Maccy/KeyboardLayout.swift
@@ -4,7 +4,10 @@ import Sauce
 class KeyboardLayout {
   static var current: KeyboardLayout { KeyboardLayout() }
 
-  var commandSwitchesToQWERTY: Bool { localizedName.contains("QWERTY ⌘") }
+  // Dvorak - QWERTY ⌘ (https://github.com/p0deje/Maccy/issues/482)
+  // bépo 1.1 - Azerty ⌘ (https://github.com/p0deje/Maccy/issues/520)
+  var commandSwitchesToQWERTY: Bool { localizedName.hasSuffix("⌘") }
+
   var localizedName: String {
     if let value = TISGetInputSourceProperty(inputSource, kTISPropertyLocalizedName) {
        return Unmanaged<CFString>.fromOpaque(value).takeUnretainedValue() as String
