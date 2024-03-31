4d2be99dde688225f034ee541442bf02b86a7393
Alex Rodionov <p0deje@gmail.com>
2024-03-30 21:25:50 -0700

    Refactor Bundle.applicationName to avoid duplication with NSWorkspace

--- Maccy/Extensions/NSWorkspace+ApplicationName.swift
+++ Maccy/Extensions/NSWorkspace+ApplicationName.swift
@@ -3,8 +3,13 @@ import AppKit
 extension NSWorkspace {
   func applicationName(url: URL) -> String {
     if let bundle = Bundle(url: url) {
-      return bundle.applicationName
+      if let displayName = bundle.object(forInfoDictionaryKey: "CFBundleDisplayName") as? String {
+        return displayName
+      } else if let name = bundle.object(forInfoDictionaryKey: "CFBundleName") as? String {
+        return name
+      }
     }
+
     return url.deletingLastPathComponent().lastPathComponent
   }
 }
