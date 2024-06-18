69faa6cb8d3a6c2c92fab7da4e1fb45228701598
Alex Rodionov <p0deje@gmail.com>
2024-06-18 09:12:55 -0700

    Migrate advanced settings to SwiftUI

--- Maccy/Extensions/NSImage+Names.swift
+++ Maccy/Extensions/NSImage+Names.swift
@@ -5,11 +5,11 @@ extension NSImage {
   static let externaldrive = NSImage(systemSymbolName: "externaldrive", accessibilityDescription: "externaldrive")
   static let paintpalette = NSImage(systemSymbolName: "paintpalette", accessibilityDescription: "paintpalette")
   static let nosign = NSImage(systemSymbolName: "nosign", accessibilityDescription: "nosign")
+  static let gearshape2 = NSImage(systemSymbolName: "gearshape.2", accessibilityDescription: "gearshape2")
 }
 
 extension NSImage.Name {
   static let clipboard = NSImage.Name("clipboard.fill")
-  static let gearshape2 = loadName("gearshape.2")
   static let maccyStatusBar = NSImage.Name("StatusBarMenuImage")
   static let pincircle = loadName("pin.circle")
   static let scissors = NSImage.Name("scissors")
---
+++ Maccy/Settings/AdvancedSettingsPane.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+import Defaults
+
+struct AdvancedSettingsPane: View {
+  var body: some View {
+    VStack(alignment: .leading) {
+      Defaults.Toggle(key: .ignoreEvents) {
+        Text("TurnOff", tableName: "AdvancedSettings")
+      }
+      Text("TurnOffDescription", tableName: "AdvancedSettings")
+        .fixedSize(horizontal: false, vertical: true)
+        .foregroundStyle(.gray)
+        .controlSize(.small)
+      Text("TurnOffShellScript", tableName: "AdvancedSettings")
+        .fixedSize(horizontal: false, vertical: true)
+        .foregroundStyle(.gray)
+        .font(.system(size: 11, design: .monospaced))
+        .controlSize(.small)
+        .padding(.vertical, 2)
+      Text("TurnOffViaMenuIconDescription", tableName: "AdvancedSettings")
+        .fixedSize(horizontal: false, vertical: true)
+        .foregroundStyle(.gray)
+        .controlSize(.small)
+      Text("TurnOffNextShellScript", tableName: "AdvancedSettings")
+        .fixedSize(horizontal: false, vertical: true)
+        .foregroundStyle(.gray)
+        .font(.system(size: 11, design: .monospaced))
+        .controlSize(.small)
+        .padding(.vertical, 2)
+
+      Divider()
+
+      Defaults.Toggle(key: .avoidTakingFocus) {
+        Text("AvoidTakingApplicationFocus", tableName: "AdvancedSettings")
+      }.help(Text("AvoidTakingApplicationFocusTooltip", tableName: "AdvancedSettings"))
+
+      Defaults.Toggle(key: .clearOnQuit) {
+        Text("ClearHistoryOnQuit", tableName: "AdvancedSettings")
+      }.help(Text("ClearHistoryOnQuitTooltip", tableName: "AdvancedSettings"))
+
+      Defaults.Toggle(key: .clearSystemClipboard) {
+        Text("ClearSystemClipboard", tableName: "AdvancedSettings")
+      }.help(Text("ClearSystemClipboardTooltip", tableName: "AdvancedSettings"))
+    }
+    .frame(minWidth: 350, maxWidth: 450)
+    .padding()
+  }
+}
+
+#Preview {
+  AdvancedSettingsPane()
+    .environment(\.locale, .init(identifier: "en"))
+}
