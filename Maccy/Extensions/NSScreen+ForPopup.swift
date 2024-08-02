7c844aa250fdf64466e7ce9391cfb1ceca7499d3
Alex Rodionov <p0deje@gmail.com>
2024-08-02 08:17:46 -0700

    Fix popup at screen center

--- Maccy/Extensions/NSScreen+ForPopup.swift
+++ Maccy/Extensions/NSScreen+ForPopup.swift
@@ -4,10 +4,10 @@ import Defaults
 extension NSScreen {
   static var forPopup: NSScreen? {
     let desiredScreen = Defaults[.popupScreen]
-    if desiredScreen == 0 || desiredScreen >= NSScreen.screens.count {
+    if desiredScreen == 0 || desiredScreen > NSScreen.screens.count {
       return NSScreen.main
     } else {
-      return NSScreen.screens[desiredScreen]
+      return NSScreen.screens[desiredScreen - 1]
     }
   }
 }
