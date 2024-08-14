e0494f3de6b3cfc67490b5ecc58200352a739c5e
Alex Rodionov <p0deje@gmail.com>
2024-08-13 22:18:38 -0700

    Fix preview delay not resetting after selecting item

--- Maccy/Views/HistoryListView.swift
+++ Maccy/Views/HistoryListView.swift
@@ -49,10 +49,10 @@ struct HistoryListView: View {
         .onChange(of: scenePhase) {
           if scenePhase == .active {
             searchFocused = true
-            appState.selection = appState.history.unpinnedItems.first?.id
-          } else {
             HistoryItemDecorator.previewThrottler.minimumDelay = Double(previewDelay) / 1000
             HistoryItemDecorator.previewThrottler.cancel()
+            appState.selection = appState.history.unpinnedItems.first?.id
+          } else {
             modifierFlags.flags = []
           }
         }
