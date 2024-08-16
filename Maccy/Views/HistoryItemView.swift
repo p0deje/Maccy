9cfec1fbec1f9ecaef60d7245343fb08427cc886
Alex Rodionov <p0deje@gmail.com>
2024-08-15 21:45:29 -0700

    Do not render `HistoryItemView` for hidden items

--- Maccy/Views/HistoryItemView.swift
+++ Maccy/Views/HistoryItemView.swift
@@ -7,22 +7,20 @@ struct HistoryItemView: View {
   @Environment(AppState.self) private var appState
 
   var body: some View {
-    if item.isVisible {
-      ListItemView(
-        id: item.id,
-        image: item.thumbnailImage ?? ColorImage.from(item.title),
-        attributedTitle: item.attributedTitle,
-        shortcuts: item.shortcuts,
-        isSelected: item.isSelected
-      ) {
-        Text(verbatim: item.title)
-      }
-      .onTapGesture {
-        appState.history.select(item)
-      }
-      .popover(isPresented: $item.showPreview, arrowEdge: .trailing) {
-        PreviewItemView(item: item)
-      }
+    ListItemView(
+      id: item.id,
+      image: item.thumbnailImage ?? ColorImage.from(item.title),
+      attributedTitle: item.attributedTitle,
+      shortcuts: item.shortcuts,
+      isSelected: item.isSelected
+    ) {
+      Text(verbatim: item.title)
+    }
+    .onTapGesture {
+      appState.history.select(item)
+    }
+    .popover(isPresented: $item.showPreview, arrowEdge: .trailing) {
+      PreviewItemView(item: item)
     }
   }
 }
