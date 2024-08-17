d227e8a1c4f1db149eb5dcfd1567390e663c1b06
Alex Rodionov <p0deje@gmail.com>
2024-08-17 08:42:26 -0700

    Support highlighting with background color a-la Safari
    Fixes #830

--- Maccy/HighlightMatch.swift
+++ Maccy/HighlightMatch.swift
@@ -2,6 +2,7 @@ import Foundation
 import Defaults
 
 enum HighlightMatch: String, CaseIterable, Identifiable, CustomStringConvertible, Defaults.Serializable {
+  case color
   case bold
   case italic
   case underline
@@ -12,6 +13,8 @@ enum HighlightMatch: String, CaseIterable, Identifiable, CustomStringConvertible
     switch self {
     case .bold:
       return NSLocalizedString("HighlightMatchBold", tableName: "AppearanceSettings", comment: "")
+    case .color:
+      return NSLocalizedString("HighlightMatchColor", tableName: "AppearanceSettings", comment: "")
     case .italic:
       return NSLocalizedString("HighlightMatchItalic", tableName: "AppearanceSettings", comment: "")
     case .underline:
