8c26f7fa99381b51da992d3fb132d138da4cdd88
Alex Rodionov <p0deje@gmail.com>
2024-09-07 15:52:48 -0700

    Fix missing description for `NSEvent.ModifierFlags`
    `description` was removed in KeyboardShortcuts 2.0.2

---
+++ Maccy/Extensions/ModifierFlags+Description.swift
@@ -0,0 +1,31 @@
+import AppKit.NSEvent
+import Carbon.HIToolbox
+
+// https://github.com/sindresorhus/KeyboardShortcuts/blob/e6b60117ec266e1e5d059f7f34815144f9762b36/Sources/KeyboardShortcuts/Utilities.swift#L308-L342
+extension NSEvent.ModifierFlags {
+  var description: String {
+    var description = ""
+
+    if contains(.control) {
+      description += "⌃"
+    }
+
+    if contains(.option) {
+      description += "⌥"
+    }
+
+    if contains(.shift) {
+      description += "⇧"
+    }
+
+    if contains(.command) {
+      description += "⌘"
+    }
+
+    if contains(.function) {
+      description += "🌐\u{FE0E}"
+    }
+
+    return description
+  }
+}
