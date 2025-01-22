f5e6cf5298fb0aaf08ba9ff3db3c075942947b69
Alex Rodionov <p0deje@gmail.com>
2025-01-22 08:13:45 -0800

    Fix SwiftLint warnings

--- Maccy/Settings/AppearanceSettingsPane.swift
+++ Maccy/Settings/AppearanceSettingsPane.swift
@@ -48,49 +48,13 @@ struct AppearanceSettingsPane: View {
     return formatter
   }()
 
-  private func labelForScreen(index screenIndex: Int) -> String {
-    switch screenIndex {
-    case 0:
-      return String(localized: "ActiveScreen", table: "AppearanceSettings")
-    case _: return screens[screenIndex - 1].localizedName
-    }
-  }
-
-  @ViewBuilder private func screenPicker(for position: PopupPosition)
-    -> some View
-  {
-    let screenBinding: Binding<Int> = Binding {
-      return popupScreen
-    } set: {
-      popupScreen = $0
-      popupAt = position
-    }
-    Picker(selection: screenBinding) {
-      Text(labelForScreen(index: 0))
-        .tag(0)
-
-      ForEach(screens.indices, id: \.self) { index in
-        Text(labelForScreen(index: index + 1))
-          .tag(index + 1)
-      }
-    } label: {
-      if popupAt == position {
-        Text("\(position.description) (\(labelForScreen(index: popupScreen)))")
-      } else {
-        Text(position.description)
-      }
-    }
-  }
-
   var body: some View {
     Settings.Container(contentWidth: 650) {
       Settings.Section(label: { Text("PopupAt", tableName: "AppearanceSettings") }) {
         HStack {
           Picker("", selection: $popupAt) {
             ForEach(PopupPosition.allCases) { position in
-              if position == .center || position == .lastPosition,
-                screens.count > 1
-              {
+              if position == .center || position == .lastPosition, screens.count > 1 {
                 screenPicker(for: position)
               } else {
                 Text(position.description)
@@ -218,6 +182,41 @@ struct AppearanceSettingsPane: View {
     }
     .onReceive(NotificationCenter.default.publisher(for: NSApplication.didChangeScreenParametersNotification)) { _ in
       screens = NSScreen.screens
+    }
+  }
+
+  @ViewBuilder
+  private func screenPicker(for position: PopupPosition) -> some View {
+    let screenBinding: Binding<Int> = Binding {
+      return popupScreen
+    } set: {
+      popupScreen = $0
+      popupAt = position
+    }
+
+    Picker(selection: screenBinding) {
+      Text(labelForScreen(index: 0))
+        .tag(0)
+
+      ForEach(screens.indices, id: \.self) { index in
+        Text(labelForScreen(index: index + 1))
+          .tag(index + 1)
+      }
+    } label: {
+      if popupAt == position {
+        Text("\(position.description) (\(labelForScreen(index: popupScreen)))")
+      } else {
+        Text(position.description)
+      }
+    }
+  }
+
+  private func labelForScreen(index screenIndex: Int) -> String {
+    switch screenIndex {
+    case 0:
+      return String(localized: "ActiveScreen", table: "AppearanceSettings")
+    case _:
+      return screens[screenIndex - 1].localizedName
     }
   }
 }
