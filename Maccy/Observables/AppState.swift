bbb0a14adfc4b10d5a1a995d2060de45d14db531
Harish <hawk3134@gmail.com>
2025-06-06 11:34:57 -0700

    Fix clear history confirmation dialogue on enter (#1109)

--- Maccy/Observables/AppState.swift
+++ Maccy/Observables/AppState.swift
@@ -70,7 +70,7 @@ class AppState: Sendable {
     if let item = history.selectedItem, history.items.contains(item) {
       history.select(item)
     } else if let item = footer.selectedItem {
-      if item.confirmation != nil {
+        if item.confirmation != nil, Defaults[.suppressClearAlert] == false {
         item.showConfirmation = true
       } else {
         item.action()
