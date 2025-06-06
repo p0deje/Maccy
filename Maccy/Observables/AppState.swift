1fc71a1d64059ac8958972599596d59bd7b2b51c
Alex Rodionov <p0deje@gmail.com>
2025-06-06 11:38:27 -0700

    Add TODO for item.suppressConfirmation

--- Maccy/Observables/AppState.swift
+++ Maccy/Observables/AppState.swift
@@ -70,7 +70,8 @@ class AppState: Sendable {
     if let item = history.selectedItem, history.items.contains(item) {
       history.select(item)
     } else if let item = footer.selectedItem {
-        if item.confirmation != nil, Defaults[.suppressClearAlert] == false {
+      // TODO: Use item.suppressConfirmation, but it's not updated!
+      if item.confirmation != nil, Defaults[.suppressClearAlert] == false {
         item.showConfirmation = true
       } else {
         item.action()
