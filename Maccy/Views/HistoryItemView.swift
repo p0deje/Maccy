472fc0a3cd2174479fffb30f79e6c00ce6aadf0a
Lai Xuan Hieu <promex04@192.168.1.50.non-exists.ptr.local>
2025-06-08 20:03:12 +0700

    Fix: Sửa lỗi import sai tên module từ 'jjecDefaults' thành 'Defaults' trong HistoryItemView.swift

--- Maccy/Views/HistoryItemView.swift
+++ Maccy/Views/HistoryItemView.swift
@@ -2,28 +2,29 @@ import Defaults
 import SwiftUI
 
 struct HistoryItemView: View {
-  @Bindable var item: HistoryItemDecorator
+    @Bindable var item: HistoryItemDecorator
 
-  @Environment(AppState.self) private var appState
+    @Environment(AppState.self) private var appState
 
-  var body: some View {
-    ListItemView(
-      id: item.id,
-      appIcon: item.applicationImage,
-      image: item.thumbnailImage ?? item.fileIcon,
-      accessoryImage: (item.thumbnailImage != nil || item.fileIcon != nil) ? nil : ColorImage.from(item.title),
-      attributedTitle: item.attributedTitle,
-      shortcuts: item.shortcuts,
-      isSelected: item.isSelected
-    ) {
-      Text(verbatim: item.text.isEmpty ? item.title : item.text)
+    var body: some View {
+        ListItemView(
+            id: item.id,
+            appIcon: item.applicationImage,
+            image: item.thumbnailImage ?? item.fileIcon,
+            accessoryImage: (item.thumbnailImage != nil || item.fileIcon != nil)
+                ? nil : ColorImage.from(item.title),
+            attributedTitle: item.attributedTitle,
+            shortcuts: item.shortcuts,
+            isSelected: item.isSelected
+        ) {
+            Text(verbatim: item.text.isEmpty ? item.title : item.text)
+        }
+        .onTapGesture {
+            appState.history.select(item)
+        }
+        .popover(isPresented: $item.showPreview, arrowEdge: .trailing) {
+            PreviewItemView(item: item)
+                .frame(idealWidth: 520, idealHeight: 750)  // Set ideal size for popover content
+        }
     }
-    .onTapGesture {
-      appState.history.select(item)
-    }
-    .popover(isPresented: $item.showPreview, arrowEdge: .trailing) {
-      PreviewItemView(item: item)
-        .frame(idealWidth: 520, idealHeight: 750) // Set ideal size for popover content
-    }
-  }
 }
