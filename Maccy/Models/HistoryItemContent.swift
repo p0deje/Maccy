cdc50cb222514cfb1a07cd2824e54cbd3b438c6b
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2025-08-25 16:08:23 +0200

    Manually delete history content

--- Maccy/Models/HistoryItemContent.swift
+++ Maccy/Models/HistoryItemContent.swift
@@ -6,6 +6,9 @@ class HistoryItemContent {
   var type: String = ""
   var value: Data?
 
+  @Relationship
+  var item: HistoryItem?
+
   init(type: String, value: Data? = nil) {
     self.type = type
     self.value = value
