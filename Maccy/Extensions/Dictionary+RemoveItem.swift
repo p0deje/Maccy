4989d8b48e54f92c21b8a9f63b27a17061ec5599
Alex Rodionov <p0deje@gmail.com>
2025-08-25 13:45:02 -0700

    Refresh storage size in settings

--- Maccy/Extensions/Dictionary+RemoveItem.swift
+++ Maccy/Extensions/Dictionary+RemoveItem.swift
@@ -1,5 +1,5 @@
 extension Dictionary {
-    /// Removes all key-value pairs where the value satisfies the given predicate.
+  // Removes all key-value pairs where the value satisfies the given predicate.
   mutating func removeValues(where shouldRemove: (Value) -> Bool) {
     for (key, value) in self where shouldRemove(value) {
       self.removeValue(forKey: key)
