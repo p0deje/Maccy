863e8405c8b32821be99e53bbe7f916786dece6c
Mithun Sridharan <Mithun.Sridharan@GMail.COM>
2025-09-01 15:21:13 +0200

    Added shortcuts "a" and "z"

--- Maccy/Models/HistoryItem.swift
+++ Maccy/Models/HistoryItem.swift
@@ -13,8 +13,8 @@ class HistoryItem {
     // "w" reserved for close window
     // "z" reserved for undo/redo
     var keys = Set([
-      "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
-      "m", "n", "o", "p", "r", "s", "t", "u", "x", "y"
+      "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+      "m", "n", "o", "p", "r", "s", "t", "u", "x", "y", "z"
     ])
 
     if let deleteKey = KeyChord.deleteKey,
