1796d94ffc6a1c59b7c20c1216dc39567fc38278
Alex Rodionov <p0deje@gmail.com>
2025-11-21 06:17:32 -0800

    Make search field corner radius similar to items

--- Maccy/Views/HeaderView.swift
+++ Maccy/Views/HeaderView.swift
@@ -19,7 +19,6 @@ struct HeaderView: View {
 
       SearchFieldView(placeholder: "search_placeholder", query: $searchQuery)
         .focused($searchFocused)
-        .clipShape(Capsule())
         .frame(maxWidth: .infinity)
         .onChange(of: scenePhase) {
           if scenePhase == .background && !searchQuery.isEmpty {
