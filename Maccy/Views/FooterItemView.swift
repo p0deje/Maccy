a166eee856a5cc3c14eaef6282eb6b8d78d5095d
Alex Rodionov <p0deje@gmail.com>
2026-01-26 07:15:06 -0800

    Hide preview when hovering over footer items

--- Maccy/Views/FooterItemView.swift
+++ Maccy/Views/FooterItemView.swift
@@ -2,11 +2,17 @@ import SwiftUI
 
 struct FooterItemView: View {
   @Bindable var item: FooterItem
+  @Environment(AppState.self) private var appState
 
   var body: some View {
     ConfirmationView(item: item) {
       ListItemView(id: item.id, selectionId: item.id, shortcuts: item.shortcuts, isSelected: item.isSelected) {
         Text(LocalizedStringKey(item.title))
+      }
+    }
+    .onHover { hovering in
+      if hovering && appState.preview.state.isOpen {
+        appState.preview.togglePreview()
       }
     }
   }
