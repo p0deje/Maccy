cbf45c24e89d6464f58733baaabeb3da8e38fa4d
Alex Rodionov <p0deje@gmail.com>
2026-01-27 21:21:56 -0800

    Switch to Control-Space to open preview

--- Maccy/Extensions/KeyboardShortcuts.Name+Shortcuts.swift
+++ Maccy/Extensions/KeyboardShortcuts.Name+Shortcuts.swift
@@ -4,5 +4,5 @@ extension KeyboardShortcuts.Name {
   static let popup = Self("popup", default: Shortcut(.c, modifiers: [.command, .shift]))
   static let pin = Self("pin", default: Shortcut(.p, modifiers: [.option]))
   static let delete = Self("delete", default: Shortcut(.delete, modifiers: [.option]))
-  static let togglePreview = Self("togglePreview", default: Shortcut(.i, modifiers: [.command]))
+  static let togglePreview = Self("togglePreview", default: Shortcut(.space, modifiers: [.control]))
 }
