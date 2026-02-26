1c0876e5aa95da256f5a5a4612b6e96332236ae7
moutella <joaogabrielmsc@gmail.com>
2026-02-26 09:36:58 -0300

    Improve grammar on comments about origin of code.

--- Maccy/Extensions/NSRunningApplication+DisplayLink.swift
+++ Maccy/Extensions/NSRunningApplication+DisplayLink.swift
@@ -1,6 +1,6 @@
 import AppKit.NSRunningApplication
 
-// Originally on Lunar.
+// Originally from Lunar.
 // https://github.com/alin23/Lunar/blob/master/Lunar/Data/Util.swift#L2401-L2409
 // To test it without displaylink, change the identifier to something else running on your computer.
 // Terminal is a good candidate, com.apple.Terminal.
