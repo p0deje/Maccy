de0143dbdff93f7b17557104a5931adff073d30c
Batu Yılmazer <yilmazerb22@itu.edu.tr>
2026-03-25 15:14:36 +0300

    fix: Pin shortcuts not working

--- Maccy/Views/KeyHandlingView.swift
+++ Maccy/Views/KeyHandlingView.swift
@@ -208,7 +208,7 @@ struct KeyHandlingView<Content: View>: View {
           ()
         }
 
-        if !searchFocused, let item = appState.history.pressedShortcutItem {
+        if let item = appState.history.pressedShortcutItem {
           appState.navigator.select(item: item)
           Task {
             try? await Task.sleep(for: .milliseconds(50))
