84462a2c45738d3e5897a4f781de835d3adeac4f
Jannis Weis <31143295+weisJ@users.noreply.github.com>
2026-07-31 11:24:22 +0200

    Avoid all computation when voice over is not enabled

--- Maccy/VoiceOver.swift
+++ Maccy/VoiceOver.swift
@@ -4,7 +4,7 @@ import AppKit
 ///
 /// Medium priority avoids interrupting rapid selection updates. Use high priority only
 /// for announcements that should interrupt current speech.
-func announceForAccessibility(_ announcement: @autoclosure () -> String, priority: NSAccessibilityPriorityLevel = .medium) {
+func announceForAccessibility(_ announcement: () -> String, priority: NSAccessibilityPriorityLevel = .medium) {
   guard NSWorkspace.shared.isVoiceOverEnabled else { return }
   NSAccessibility.post(
     element: NSApp as Any,
